1aa18a1f320c0cff9c51f174f1d7e92b0a66ad3f
ahmed-elrooby <ahmdalrwby24@gmail.com>
2026-07-13 01:05:00 +0300

    dddd

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import DbConnect from "./databases/dbConnect.js";
 import { init } from "./src/index.routes.js";
+import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
-import swaggerSpec from "./swagger/swaggerDocs.js";
+
 const port = process.env.PORT || 3000;
 const app = express();
 
@@ -13,13 +14,43 @@ app.use(express.json());
 app.use("/", express.static("uploads/"));
 app.set("query parser", "extended");
 
+// Swagger Configuration
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Noon API",
+      version: "1.0.0",
+      description: "Noon E-commerce API Documentation",
+    },
+    servers: [
+      {
+        url: "https://noon-six.vercel.app/api/v1",
+        description: "Production Server",
+      },
+      {
+        url: `http://localhost:${port}/api/v1`,
+        description: "Local Server",
+      },
+    ],
+  },
+
+  // اقرأ كل ملفات الـ YAML الموجودة داخل docs
+  apis: ["./docs/*.yaml"],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 init(app);
+
 await DbConnect();
 
 if (process.env.NODE_ENV !== "production" && !process.env.VERCEL) {
-  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+  });
 }
 
 process.on("unhandledRejection", (err) => console.log(err));
--- src/router/auth.router.js
+++ src/router/auth.router.js
@@ -1,34 +1,6 @@
 import express from "express";
 import { signIn, signUp } from "../controller/auth/auth.controller.js";
 const authRouter = express.Router();
-/**
- * @openapi
- * /auth/signup:
- *   post:
- *     tags:
- *       - Auth
- *     summary: Register a new user
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - name
- *               - email
- *               - password
- *             properties:
- *               name:
- *                 type: string
- *               email:
- *                 type: string
- *               password:
- *                 type: string
- *     responses:
- *       201:
- *         description: User created successfully
- */
 
 authRouter.post("/signUp", signUp);
 authRouter.post("/signIn", signIn);
