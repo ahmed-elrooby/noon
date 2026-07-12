bf0180cb5f69467aa692702c6dbd6691392e3907
ahmed-elrooby <ahmdalrwby24@gmail.com>
2026-07-13 01:46:56 +0300

    gfg

--- server.js
+++ server.js
@@ -20,36 +20,19 @@ app.set("query parser", "extended");
 const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
-
     info: {
       title: "Noon API",
       version: "1.0.0",
       description: "E-commerce API Documentation",
     },
-
     servers: [
       {
         url: "https://noon-coral.vercel.app/api/v1",
-        description: "Production Server",
-      },
-      {
-        url: `http://localhost:${port}/api/v1`,
-        description: "Local Server",
       },
     ],
-
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: "apiKey",
-          in: "header",
-          name: "token",
-        },
-      },
-    },
   },
 
-  apis: ["./docs/*.yaml"],
+  apis: [`${process.cwd()}/docs/*.yaml`],
 };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
